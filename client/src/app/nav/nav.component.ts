b3f4c950726aaeccafafef6193ef771052baf449
SamuelFutur <Samuel.Yao@futurafric.com>
2024-02-10 01:02:33 +0000

    fifth commit

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -14,7 +16,7 @@ export class NavComponent implements OnInit{
   }
  
   
-  constructor (public accountServices: AccountService){}
+  constructor (public accountServices: AccountService, private router: Router, private toastr: ToastrService){}
   ngOnInit(): void {
     
   }
@@ -23,16 +25,15 @@ export class NavComponent implements OnInit{
 
   login(){
     this.accountServices.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
-        
-      },
-      error: error => console.log(error)
+      next: () =>  this.router.navigateByUrl('/members'),
+      error: error =>{ this.toastr.error(error.error);
+        console.log(error);}
     })
   }
 
   logout(){
     this.accountServices.logout();
+    this.router.navigateByUrl('/');
     
   }
 }
