c7c92e14e3f28b356eac26e04d53575ab8a9df6e
SamuelFutur <Samuel.Yao@futurafric.com>
2024-04-15 01:33:29 +0000

    10th

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Security.Claims;
 using API.DTOs;
 using API.Interfaces;
 using AutoMapper;
@@ -34,10 +35,19 @@ public class UsersController : BaseApiController
  public async  Task<ActionResult<MemberDto>> GetUser(string username)
  {
     return await  _userRepository.GetMemberAsync(username);
-
-    
-
    
+ }
+
+ [HttpPut]
+ public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+ {
+   var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+   var user = await _userRepository.GetUserBYUsernameAsync(username);
+   if(user==null) return NotFound();
+   _mapper.Map(memberUpdateDto, user);
+   if(await _userRepository.SaveAllAsync()) return NoContent();
+
+   return BadRequest("Failed to update user");
  }
 
 
